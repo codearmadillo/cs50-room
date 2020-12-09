fded407ecc6bf95eb41cb7fe5ba27c29883398b5
Jiří Královec <36158327+jiriKralovec@users.noreply.github.com>
2020-12-09 22:37:39 +0100

    Added masks to static objects

--- src/classes/rooms/first.ts
+++ src/classes/rooms/first.ts
@@ -3,11 +3,6 @@ import { IRoom } from "../../interfaces/room";
 import { Table } from "../objects/table";
 
 export class FirstRoom extends GenericRoom implements IRoom {
-  constructor(
-    protected readonly window_width : number, protected readonly window_height : number
-  ) {
-    super(window_width, window_height);
-  }
   draw() {
     super.draw();
   }
