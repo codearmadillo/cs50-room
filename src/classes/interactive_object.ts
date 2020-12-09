8a168d9f4c83c4fe472a3a3f9e6c3bdf74ea7ae4
Jiří Královec <36158327+jiriKralovec@users.noreply.github.com>
2020-12-09 23:40:45 +0100

    Added new interactive object class

---
+++ src/classes/interactive_object.ts
@@ -0,0 +1,25 @@
+import { BouncingBoxConstraints } from "../types/boucing-box";
+import { GameObject } from "./game-object";
+
+export abstract class InteractiveObject extends GameObject {
+  public get interaction_box() : BouncingBoxConstraints {
+    return {
+      x1: this.x - 5,
+      y1: this.y - 5,
+      x2: this.x + this.width + 5,
+      y2: this.y - this.width - 10
+    }
+  }
+  draw() { }
+  draw_interaction_box(color : [number, number, number, number] = [ 1, 0, 1, .2 ]) {
+    love.graphics.setColor(color);
+    love.graphics.polygon(
+      'fill',
+      this.interaction_box.x1, this.interaction_box.y1,
+      this.interaction_box.x2, this.interaction_box.y1,
+      this.interaction_box.x2, this.interaction_box.y2,
+      this.interaction_box.x1, this.interaction_box.y2,
+      this.interaction_box.x1, this.interaction_box.y1,
+    )
+  }
+}
