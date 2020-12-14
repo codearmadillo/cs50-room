f46c0ba0a224521bc9b1c69eebae4a1ee41509c6
Jiří Královec <36158327+jiriKralovec@users.noreply.github.com>
2020-12-14 18:11:38 +0100

    Re-enables player z-index

--- src/controllers/game-controller.ts
+++ src/controllers/game-controller.ts
@@ -47,13 +47,12 @@ export class Game {
         object.draw();
       } else {
         if(!isPlayerRendered) {
-          //this.player.draw();
+          this.player.draw();
           isPlayerRendered = true;
         }
         object.draw();
       }
     });
-    this.player.draw();
     /** Light pass */
     // this.room.draw_lightmask();
     /** Draw action for interactive objects */
